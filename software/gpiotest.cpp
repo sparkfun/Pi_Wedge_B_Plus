820b901aa31b3c76c1dbefd9966a04860c9f83ca
Jacquot-SFE <byron.jacquot@sparkfun.com>
2014-12-18 14:22:52 -0700

    Software updates
    Updating for extended pins on B+.

--- software/gpiotest.cpp
+++ software/gpiotest.cpp
@@ -28,16 +28,18 @@ To build this file, I use the command:
 Then to run it, I have to use sudo, as it creates restricted file resources.
 > sudo ./a.out
 
-WiringPi defines its own pin numbers, in a contiguous range, from 0 to 16.  
-These numbers account for the V1/V2 hardware differences.  You can alternately 
-use the RPi/Broadcom pin numbers.
+WiringPi defines its own pin numbers, in a contiguous range.  On a B+,
+this range is 0 to 31.
+You can alternately use the RPi/Broadcom pin numbers.
 
-This test will repurpose the UART pins.  If you're using a terminal on the UART, 
-constrain it to pins 0 through 14.  If you're using SSH/telnet, you can include 
-pins 15 and 16.
+This test will configure the pins it tests as GPIO outputs.  If you're using
+UART, SPI or I2C peripherals, beware that this test will reconfigure those pins.
+In particular, it can sever a UART connection.  If you're using a command line
+over the serial port, Don't let it hit pins 15 and 16!
+If you're using SSH/telnet, you can simply walk from from 0 to 31.
 
 Development environment specifics:
-Tested on Raspberry Pi V2 hardware, running Raspbian.
+Tested on Raspberry Pi B+ hardware, running Raspbian.
 Building with GCC 4.6.3 (Debian 4.6.3-14+rpi1)
 
 This code is beerware; if you see me (or any other SparkFun employee) at the
@@ -55,7 +57,13 @@ using namespace std;
 // This will only touch 0 through 14.  15 and 16 are the UART, which we 
 // wouldn't want to reconfigure is you're using them for a terminal.
 // If you're using SSH, you can run up to 16.
+static const int FIRST_PIN = 0;
 static const int MAX_PIN = 14;
+
+// For a B+, we also have pins 17 through 31.
+// Use these instead, if you want to test the pins above the UART.
+//static const int FIRST_PIN = 17;
+//static const int MAX_PIN = 31;
 
 int main()
 {
@@ -75,7 +83,7 @@ int main()
    cout << "Setup result: " << result << endl;
 
    // set all pins as outputs
-   for(int i = 0; i <= MAX_PIN; i++)
+   for(int i = FIRST_PIN; i <= MAX_PIN; i++)
    {
       pinMode(i, OUTPUT);
 
@@ -85,9 +93,9 @@ int main()
    // Cycle through the pins, setting one at a time high.
    // Repeat 10 times.
    // This allows us to check them with a meter or oscilloscope.
-   for(int i = 0; i < MAX_PIN; i++)
+   for(int i = FIRST_PIN; i < MAX_PIN; i++)
    {
-      for(int j = 0; j <= MAX_PIN; j++)
+      for(int j = FIRST_PIN; j <= MAX_PIN; j++)
       {
          if( (i % 17) == j)
          {
